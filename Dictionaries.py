a2f69eff1f9ffb292393f1b7381232df8d31658f
HuuzaifaAhmed <huuzaifaahmad@gmail.com>
2024-08-22 16:04:06 +0500

    Add files via upload

---
+++ Dictionaries.py
@@ -0,0 +1,11 @@
+intro={
+    "Name":"Huzaifa Ahmed",
+    "Class":"2nd yaer",
+    "Section":"3",
+    "intrest":"in cars",
+    "driving experience":"6 years",
+    "age":"17",
+}
+
+for i , el in intro.items():
+    print(i,":",el)
